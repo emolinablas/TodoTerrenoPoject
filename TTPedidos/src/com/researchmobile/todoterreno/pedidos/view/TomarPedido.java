1f82f9fa788fdd5097df42ce6ad6378b8b190cdc
Ever Molina <moulinaem@gmail.com>
2012-12-20 12:58:39 -0600

    COMMIT DE CLASES
    Clases para el primer commit del proyecto

---
+++ TTPedidos/src/com/researchmobile/todoterreno/pedidos/view/TomarPedido.java
@@ -0,0 +1,72 @@
+package com.researchmobile.todoterreno.pedidos.view;
+
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.researchmobile.todoterreno.pedidos.entity.Cliente;
+import com.researchmobile.todoterreno.pedidos.entity.DetallePedido;
+import com.researchmobile.todoterreno.pedidos.entity.EncabezadoPedido;
+import com.researchmobile.todoterreno.pedidos.entity.NuevoPedido;
+import com.researchmobile.todoterreno.pedidos.entity.User;
+import com.researchmobile.todoterreno.pedidos.utility.ConnectState;
+import com.researchmobile.todoterreno.pedidos.utility.Fecha;
+import com.researchmobile.todoterreno.pedidos.utility.FormatDecimal;
+
+public class TomarPedido extends Activity implements TextWatcher, OnItemClickListener {
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tomar_pedido);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterTextChanged(Editable arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
