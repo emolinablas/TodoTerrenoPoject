d8476a34948478947312d91fb9d2a0c5f06e0239
Wuilder Alvarado <walvarado@researchmobile.co>
2013-02-14 13:59:33 -0600

    Preparando Clases para Facturacion
    Se estan creando paquetes y clases para el módulo de Facturación

---
+++ TTPedidos/src/com/researchmobile/todoterreno/pedidos/ws/RequestWSFactura.java
@@ -0,0 +1,13 @@
+package com.researchmobile.todoterreno.pedidos.ws;
+
+public class RequestWSFactura {
+	private static String WS_LOGIN = "ws_login.php?a=login&";
+	private static String WS_CLIENTES = "ws_clientes.php?";
+	private static String WS_PRODUCTOS = "ws_articulos.php?a=catalogo&idportafolio=";
+	private static String WS_ENVIO = "json.php?username=";
+	private static String WS_ENVIAMOTIVO = "ws_noventa.php?cliente=";
+	private static String WS_CATEGORIAS = "ws_categorias.php?";
+	
+	
+
+}
